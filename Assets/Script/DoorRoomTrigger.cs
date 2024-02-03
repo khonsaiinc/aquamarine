68412630e290268a3e409d4a9fdca272d54250c5
GGameover303 <ggameover303@gmail.com>
2024-02-03 22:35:45 +0700

    RepairSecene

---
+++ Assets/Script/DoorRoomTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace DS
+{
+    public class DoorRoomTrigger : MonoBehaviour
+    {
+        
+        [SerializeField] GameObject door;
+
+        void OnTriggerStay2D(Collider2D coll)
+        {
+            if(coll.tag == "Player")
+            {
+                door.SetActive(true);
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+        void OnTriggerExit2D(Collider2D coll)
+        {
+                door.SetActive(false);
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
