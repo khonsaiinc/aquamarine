dd57dcb0d5a5ff5d61eb96a8b2449c83734fa4aa
GGameover303 <ggameover303@gmail.com>
2024-02-17 19:06:55 +0700

    Add Cutscene after Start in MainMenu

--- Assets/Script/SceneUI.cs
+++ Assets/Script/SceneUI.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 public class ScenceUI : MonoBehaviour
 {
+    [Header("UI & Buttons",order = 0)]
+    [SerializeField] GameObject button;
+    [SerializeField] GameObject backgroundScreen;
+
+    [Header("VideoPlayer",order = 1)]
+    [SerializeField] VideoPlayer videoPlayer;
+    void Start()
+    {
+        videoPlayer.playOnAwake = false;
+    }
+
     public void NextScence()
     {
+        StartCoroutine(isPlayingCutscene());
+    }
+
+    IEnumerator isPlayingCutscene()
+    {
+        button.SetActive(false);
+        
+        yield return new WaitForSeconds(1f);
+        backgroundScreen.SetActive(false);
+
+        videoPlayer.Play();
+
+        yield return new WaitForSeconds(22f);
+
         SceneManager.LoadScene("Room");
     }
 }
