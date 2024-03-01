e6bb66026f66653b4c97e1572ae84306b8d0cd07
GGameover303 <ggameover303@gmail.com>
2024-03-01 19:11:43 +0700

    TakedaMoveOutScene and OldladyMoveToCashier

--- Assets/Script/TakedaOutside.cs
+++ Assets/Script/TakedaOutside.cs
@@ -1,10 +1,13 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 
 public class TakedaOutside : MonoBehaviour
 {
     [SerializeField] GameObject doorSuperMarket;
     [SerializeField] GameObject triggerTakeda;
+    [SerializeField] PlayableDirector playableDirector;
     void Start()
     {
         if(QuestCheck.questTalkTakeda){
@@ -17,6 +20,13 @@ public class TakedaOutside : MonoBehaviour
         if(QuestCheck.questTalkTakeda){
             triggerTakeda.SetActive(false);
             doorSuperMarket.SetActive(true);
+        StartCoroutine(TakadaGoOutScene());
         }
+    }
+
+    IEnumerator TakadaGoOutScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+        playableDirector.Play();
     }
 }
