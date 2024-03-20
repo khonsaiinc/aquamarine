f2c24c9c4f57cd0356dd38a5a58ebe8fe461714a
Chitsanupong Chancluab <beersanp@duck.com>
2024-03-20 18:58:19 +0700

    delete old video, and change commit submodule

--- Assets/Script/SceneUI.cs
+++ Assets/Script/SceneUI.cs
@@ -1,39 +1,17 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
-
-public class ScenceUI : MonoBehaviour
-{
-    [Header("UI & Buttons",order = 0)]
-    [SerializeField] GameObject button;
-    [SerializeField] GameObject backgroundScreen;
-
-    [Header("VideoPlayer",order = 1)]
-    [SerializeField] VideoPlayer videoPlayer;
-    void Start()
-    {
-        videoPlayer.playOnAwake = false;
-    }
-
-    public void NextScence()
-    {
-        StartCoroutine(isPlayingCutscene());
-    }
-
-    IEnumerator isPlayingCutscene()
-    {
-        button.SetActive(false);
-        
-        yield return new WaitForSeconds(1f);
-        backgroundScreen.SetActive(false);
-
-        videoPlayer.Play();
-
-        yield return new WaitForSeconds(22f);
-
+public class SceneUI : MonoBehaviour {
+    public void loadGameSceneFirst(){
         SceneManager.LoadScene("Room");
     }
-}
 
+    public void soundManagement(){
+        // sound here
+    }
+
+    public void exitGame(){
+        Debug.Log("application exiting...");
+        Application.Quit();
+    }
+}
