23e98be40b8d61c4012f732bb7904aa2a4dcdbd2
Chitsanupong Chancluab <beersanp@duck.com>
2024-03-21 17:04:43 +0700

    it's real update for sceneui

--- Assets/Script/SceneUI.cs
+++ Assets/Script/SceneUI.cs
@@ -7,11 +7,16 @@ public class SceneUI : MonoBehaviour {
     }
 
     public void soundManagement(){
-        // sound here
+        // * need to do it, @room25552555, it's for main menu need to disable here
     }
 
     public void exitGame(){
-        Debug.Log("application exiting...");
-        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            Debug.Log("Application Exiting..");
+        #else
+            Application.Quit();
+            Debug.Log("Application Exit..");
+        #endif
     }
 }
