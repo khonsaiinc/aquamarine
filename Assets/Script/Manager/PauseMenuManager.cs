4212f1844e14c2954b5c14109217b693b4c92568
Chitsanupong Chancluab <beersanp@duck.com>
2024-03-21 17:02:24 +0700

    update main menu script
    add if(region) for debug in unity

---
+++ Assets/Script/Manager/PauseMenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
